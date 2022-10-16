43bc0e28900f63ea435d6f1f04acb159de518826
runyan-garba-55 <Runyangab@gmail.com>
2022-10-16 15:44:53 -0700

    4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*
+ * main - use puts to print a code line
+ *
+ * return: 0
+*/
+int main(void) 
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}	
