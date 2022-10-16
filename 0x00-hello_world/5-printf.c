63986b43490c3e68a0718090df3c4f5548b4240f
runyan-garba-55 <Runyangab@gmail.com>
2022-10-16 16:35:11 -0700

    5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - use printf to print a c program
+ *
+ * return: 0
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
